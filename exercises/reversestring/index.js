23b2947b0d40013858b24c65da41ceee05c6bde4
Alex K <damned182@mail.ru>
2019-07-16 17:59:52 +0300

    resolve reversestring

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,19 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  return str.split('').reduce((s, c) => c + s, '');
+
+  // if (str.length <= 1) return str;
+  //
+  // let s = '';
+  // for (let i = 0; i < str.length; i++) {
+  //   s = str[i] + s;
+  // }
+  //
+  // return s;
+
+  // return str.split('').reverse().join('');
+}
 
 module.exports = reverse;
