5745ae3f50b05a9edd66c2d76fcaa276f98c9122
Alex K <damned182@mail.ru>
2019-07-16 19:30:15 +0300

    solve maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -4,7 +4,30 @@
 // --- Examples
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
+function getCharMap(str) {
+  const charMap = {};
 
-function maxChar(str) {}
+  for (let char of str) {
+    charMap[char] = charMap[char] + 1 || 1;
+  }
+
+  return charMap;
+}
+
+function maxChar(str) {
+  const charMap = getCharMap(str);
+
+  let max = 0;
+  let resChar = '';
+
+  for (let char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[char];
+      resChar = char;
+    }
+  }
+
+  return resChar;
+}
 
 module.exports = maxChar;
