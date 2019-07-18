27eb1541387d847683501fe9215623bedb70509c
Alex K <damned182@mail.ru>
2019-07-18 21:57:22 +0300

    solve pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,44 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n, row = 0, str = '') {
+  if (row === n) return;
+
+  const max = n * 2 - 1;
+  if (str.length === max) {
+    console.log(str);
+    return pyramid(n, row + 1);
+  }
+
+  const midLeft = Math.floor(max / 2) - row;
+  const midRight = Math.floor(max / 2) + row;
+
+  if (str.length >= midLeft && str.length <= midRight) {
+    str += '#';
+  } else {
+    str += ' ';
+  }
+
+  pyramid(n, row, str);
+
+
+
+  // const max = n * 2 - 1;
+  //
+  // for (let row = 0; row < n; row++) {
+  //   let str = '';
+  //   const midRight = Math.floor(max / 2) + row;
+  //   const midLeft = Math.floor(max / 2) - row;
+  //
+  //   for (let col = 0; col < max; col++) {
+  //     if (col >= midLeft && col <= midRight) {
+  //       str += '#';
+  //     } else {
+  //       str += ' ';
+  //     }
+  //   }
+  //   console.log(str);
+  // }
+}
 
 module.exports = pyramid;
